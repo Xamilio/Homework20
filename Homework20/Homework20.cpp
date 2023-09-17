0bdd361338f31ee1efebcfb4258af2ec11eba169
user <user@WIN-4HU5T7LM44J>
2023-09-17 21:09:53 +0300

    Добавьте файлы проекта.

---
+++ Homework20/Homework20.cpp
@@ -0,0 +1,58 @@
+﻿#include<iostream>\
+using namespace std;
+
+//1
+#define h1(a, b) ((a) % (b) == 0 )
+ 
+//2
+#define h2(x, r) \
+  (r) = 0;\
+  for (int i = 0; i < (x); i++) \
+  { \
+		(r) = (r) + i; \
+  }\
+
+//3 
+#define h3(l, y, r)  \
+  r = 1;\
+  for (int i = 0; i < (y); i++) \
+  { \
+  	(r) = (r) * (l); \
+  } \
+
+//1
+//int main()
+//{
+//	int a = 30, b = 2;
+//	if (h1(a, b))
+//	{
+//		cout << "True";
+//	}
+//	else
+//	{
+//		cout << "False";
+//	}
+//}
+
+//2
+//int main()
+//{
+//	int r = 0;
+//	int x;
+//	cout << "Введите число которое больше 1: ";
+//	cin >> x;
+//	h2(x, r);
+//	cout << r;
+//	
+//}
+
+//3
+// 
+//int main()
+//{
+//	int l = 5;
+//	int y = 4;
+//	int r = 1;
+//	h3(l, y, r);
+//	cout << r;
+//}
